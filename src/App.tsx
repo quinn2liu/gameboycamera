d8c6c3c4827811b8808d6f8b5e85ca5179249f1f
Quinn Liu <quinn2liu@gmail.com>
2024-07-07 21:49:48 -0400

    pixelation works except for edges of images

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,37 @@
-import { useState } from 'react'
-import { pixelateImage } from './ImageConversion.tsx'
+import { cropImage, pixelateImage } from './ImageConversion.tsx'
 
 export default function App() {
 
   const demoFilePath = '/gameboy.jpg';
 
-  const cropImage = (originalImage: HTMLImageElement, centerX: number, centerY: number, edgeLength: number): Promise<HTMLImageElement> => {
-    return new Promise((resolve, reject) => {
-      const canvas = document.createElement('canvas');
-      canvas.width = edgeLength;
-      canvas.height = edgeLength;
-      const context = canvas.getContext('2d');
-
-      if (!context) {
-          throw new Error('Failed to get 2d context');
-      }
-      const croppedImageElement = new Image();
-
-      // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-      // cropping the original image by drawing it with new dimensions onto the canvas
-      originalImage.onload = () => {
-        try {
-          context.drawImage(originalImage, centerX - Math.floor((edgeLength / 2)), centerY - Math.floor((edgeLength / 2)), edgeLength, edgeLength, 0, 0, edgeLength, edgeLength);
-          const croppedImageUrl = canvas.toDataURL();
-          croppedImageElement.src = croppedImageUrl;
-          resolve(croppedImageElement);
-        } catch (error) {
-          reject(error);
-        }
-      };
-    })
-    
-
-  }
-
   const handleButton = async () => {
     try {
       const originalImage = new Image();
       originalImage.src = demoFilePath;
-      const editedImage = await cropImage(originalImage, 600, 337, 675);
-      const displayElement = document.getElementById('editedImage');
-      if (displayElement) {
-        displayElement.src = editedImage.src;
+
+      const croppedImageElement = await cropImage(originalImage, 600, 337, 675);
+      // const croppedTag = document.getElementById('croppedTag');
+      // if (croppedTag) {
+      //   croppedTag.src = croppedImageElement.src;
+      // }
+
+      const pixelatedImageElement = await pixelateImage(croppedImageElement, 675);
+      const pixelatedTag = document.getElementById('pixelatedTag');
+      if (pixelatedTag) {
+        pixelatedTag.src = pixelatedImageElement.src;
       }
+
     } catch (error) {
       console.error(error);
     }
   }
 
-  
 
   return (
     <>
       <button onClick={handleButton}>Do Something</button>
       <img src={demoFilePath} alt='' />
-      <img id='editedImage' src=''/>
+      <img id='pixelatedTag' src='' />
     </>
 
   )
--- src/ImageConversion.tsx
+++ src/ImageConversion.tsx
@@ -7,53 +7,82 @@
  * @param {number} edgeLength
  */
 // originalImage, centerX = 600, centerY = 337, edgeLength = 675
-const pixelateImage = (originalImage: HTMLImageElement, centerX: number, centerY: number, edgeLength: number): HTMLImageElement => {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
+const pixelateImage = (croppedImage: HTMLImageElement, edgeLength: number): Promise<HTMLImageElement> => {
+    return new Promise((resolve, reject) => {
+        const canvas = document.createElement('canvas');
+        canvas.width = edgeLength;
+        canvas.height = edgeLength;
+        const context = canvas.getContext('2d');
+        const pixelatedImageElement = new Image();
 
-    if (!context) {
-        throw new Error('Failed to get 2d context');
-    }
-
-    // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-    // cropping the original image by drawing it with new dimensions onto the canvas
-    context.drawImage(originalImage, centerX - (Math.floor((edgeLength / 2)), centerY - (Math.floor((edgeLength / 2)), edgeLength, edgeLength, 0, 0, edgeLength, edgeLength);
-    
-
-    // getImageData returns an ImageData object representing pixel data of the selected canvas. the .data attribute returns an Uint8ClampedArray of pixels
-    const originalImageData = context.getImageData(0, 0, edgeLength, edgeLength).data;
-
-    const pixelationFactor = Math.floor(edgeLength / 128);
-
-    // each iteration represents drawing of each of the 128 pixels 
-    for (let x = 0; x < 128; x += 1) {
-        for (let y = 0; y < 128; y += 1) {
-            
-            // calculates the index in the originalImageData array that represents the current pixel: ((x * pixelationFactor) + (y * pixelationFactor * edgeLength)) * 4;
-            const pixelIndex = 4 * pixelationFactor * (x + y * edgeLength);
-
-            // context.fillStyle sets the color for the context. assigned to the currently-iterated pixel
-            context.fillStyle = `rgba(
-                ${originalImageData[pixelIndex]},
-                ${originalImageData[pixelIndex + 1]},
-                ${originalImageData[pixelIndex + 2]},
-                ${originalImageData[pixelIndex + 3]},
-            )`;
-            // fillRect(x, y, width, height)
-            context.fillRect(x, y, pixelationFactor, pixelationFactor);
+        if (!context) {
+            throw new Error('Failed to get 2d context');
         }
-    }
-    const pixelatedImageURL = canvas.toDataURL();
-    const pixelatedImageElement = new Image();
-    pixelatedImageElement.src = pixelatedImageURL;
-    return pixelatedImageElement;
-}
-/**
- * Description placeholder
- */
-function getCropCoord() {
-    // TO DO 
-    // (gets coordinates for where to crop the image)
+
+        const pixelationFactor = Math.floor(edgeLength / 128);
+
+        console.log(pixelationFactor);
+
+        croppedImage.onload = () => {
+            try {
+                // redrawing the croppedImage on a canvas to extract data
+                context.drawImage(croppedImage, 0, 0);
+                const croppedImageData = context.getImageData(0, 0, edgeLength, edgeLength).data;
+                const pixelationFactor = Math.floor(edgeLength / 128);
+                // each iteration represents drawing of each of the 128 pixels 
+                for (let y = 0; y < 128; y += 1) {
+                    for (let x = 0; x < 128; x += 1) {
+                        
+                        // calculates the index in the originalImageData array that represents the current pixel: ((x * pixelationFactor) + (y * pixelationFactor * edgeLength)) * 4;
+
+                        const pixelIndex = 4 * pixelationFactor * (x + y * edgeLength);
+
+                        // context.fillStyle sets the color for the context. assigned to the currently-iterated pixel
+                        context.fillStyle = `rgba(
+                            ${croppedImageData[pixelIndex]},
+                            ${croppedImageData[pixelIndex + 1]},
+                            ${croppedImageData[pixelIndex + 2]},
+                            ${croppedImageData[pixelIndex + 3] / 255}
+                        )`;
+                        // fillRect(x, y, width, height)
+                        context.fillRect(x * pixelationFactor, y * pixelationFactor, pixelationFactor, pixelationFactor);
+                    }
+                }
+                const pixelatedImageURL = canvas.toDataURL();
+                pixelatedImageElement.src = pixelatedImageURL;
+                resolve(pixelatedImageElement);
+            } catch (error) {
+                reject(error);
+            }
+        }
+    });
 }
 
-export { pixelateImage };
+const cropImage = (originalImage: HTMLImageElement, centerX: number, centerY: number, edgeLength: number): Promise<HTMLImageElement> => {
+    return new Promise((resolve, reject) => {
+      const canvas = document.createElement('canvas');
+      canvas.width = edgeLength;
+      canvas.height = edgeLength;
+      const context = canvas.getContext('2d');
+
+      if (!context) {
+          throw new Error('Failed to get 2d context');
+      }
+      const croppedImageElement = new Image();
+
+      // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+      // cropping the original image by drawing it with new dimensions onto the canvas
+      originalImage.onload = () => {
+        try {
+          context.drawImage(originalImage, centerX - Math.floor((edgeLength / 2)), centerY - Math.floor((edgeLength / 2)), edgeLength, edgeLength, 0, 0, edgeLength, edgeLength);
+          const croppedImageUrl = canvas.toDataURL();
+          croppedImageElement.src = croppedImageUrl;
+          resolve(croppedImageElement);
+        } catch (error) {
+          reject(error);
+        }
+      };
+    });
+  }
+
+export { cropImage, pixelateImage };
